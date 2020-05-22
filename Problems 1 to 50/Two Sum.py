76f872b60a2ae09cb9a087f4551560c443ae54bd
Andrew <32670729+anzdrew@users.noreply.github.com>
2020-05-22 02:35:07 +0100

    Create Two Sum.py

---
+++ Problems 1 to 50/Two Sum.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(i, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+            
