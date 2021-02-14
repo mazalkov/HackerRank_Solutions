72661141c8dc39b3d2c8ed0230c8686485a7c6a5
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-02-14 17:56:22 +0000

    Create Functions.js

---
+++ 10 Days of JavaScript/Day 1/Functions.js
@@ -0,0 +1,48 @@
+// https://www.hackerrank.com/challenges/js10-function/
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+
+function factorial(n) {
+    
+    let answer = 1;
+    
+    while (n > 1) {
+        answer *= n;
+        n--;
+    }
+    
+    return answer;
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
