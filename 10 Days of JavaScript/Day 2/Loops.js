af54937636ed81bd349d3d8ac0928b55073d5b14
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-02-16 12:29:26 +0000

    Update Loops.js

--- 10 Days of JavaScript/Day 2/Loops.js
+++ 10 Days of JavaScript/Day 2/Loops.js
@@ -1,4 +1,52 @@
 // https://www.hackerrank.com/challenges/js10-loops/
 
 
-// TODO
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    
+    let vowels = 'aeiou';
+    
+    for (let v of s) {
+        if (vowels.includes(v)) 
+            console.log(v);
+    }
+    
+    for (let v of s) {
+        if (!vowels.includes(v))
+            console.log(v);
+    }
+}
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
