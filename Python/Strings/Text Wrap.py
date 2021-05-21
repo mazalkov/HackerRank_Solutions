e478563e42a589362d72d514c25db3f82cbb64fc
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-05-21 12:02:55 +0100

    Create Text Wrap.py

---
+++ Python/Strings/Text Wrap.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/text-wrap/
+
+
+import textwrap
+
+def wrap(string, max_width):
+    
+    wraps = textwrap.fill(string, max_width)
+    
+    return wraps
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
