b99b965d93eeea5eef1721d84f23b1e951957668
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-05-31 12:25:48 +0100

    Create Set add.py

---
+++ Python/Sets/Set add.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/py-set-add/
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+
+s = set()
+
+for i in range(n):
+    country = input()
+    s.add(country)
+    
+print(len(s))
